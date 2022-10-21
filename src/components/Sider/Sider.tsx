9af4d15026c9d1dbb7ab155ae26adf587b2b7868
b0nz <elliantosarono.p@gmail.com>
2022-10-22 06:51:42 +0700

    style: change font size

--- src/components/Sider/Sider.tsx
+++ src/components/Sider/Sider.tsx
@@ -10,7 +10,7 @@ const Sider: FC = () => {
         </div>
         <div className="flex flex-col pl-9 gap-3">
           <h1 className="text-[2rem] font-popins font-semibold">Welcome to Prisgn</h1>
-          <p className="text-sm font-roboto">
+          <p className="text-base font-roboto font-light">
             Is a personal data platform, you can update your information about
             yourself, customize your profile and change a lot of things
           </p>
