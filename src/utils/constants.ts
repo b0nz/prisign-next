101325c570e2b0eebfc726cded00dd640e46e845
b0nz <elliantosarono.p@gmail.com>
2022-10-25 03:20:27 +0700

    update constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const BASE_URL =
   process.env.NEXT_PUBLIC_MODE === 'development'
-    ? 'http://localhost:8010/proxy'
+    ? process.env.NEXT_PUBLIC_PROXY_URL
     : process.env.NEXT_PUBLIC_BASE_URL
-export const RE_DIGIT = new RegExp(/^\d+$/);
+export const RE_DIGIT = new RegExp(/^\d+$/)
